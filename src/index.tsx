f574de7ba04e7279bb3e63d70c1af1e3f6751113
Thiago Pereira <thiagopereiramail@gmail.com>
2022-11-25 07:02:02 -0300

    Configure the project structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,22 @@ import "./index.css";
 import Icon from "./assets/imgs/icon.png";
 import { canvas } from "./ui/canvas";
 
-function printMe() {
-  console.log("Imprima algo diferente!");
-}
+window.addEventListener("load", () => {
+  function printMe() {
+    console.log("Imprima algo diferente!");
+  }
 
-const div = document.createElement("div");
-const icon = new Image();
-icon.src = Icon;
+  const div = document.createElement("div");
+  const icon = new Image();
+  icon.src = Icon;
 
-const btn = document.createElement("button");
-btn.innerText = "Click here!";
-btn.onclick = printMe;
+  const btn = document.createElement("button");
+  btn.innerText = "Click here!";
+  btn.onclick = printMe;
 
-div.appendChild(canvas);
-div.appendChild(icon);
-div.appendChild(btn);
+  div.appendChild(canvas);
+  div.appendChild(icon);
+  div.appendChild(btn);
 
-document.body.appendChild(div);
+  document.body.appendChild(div);
+});
